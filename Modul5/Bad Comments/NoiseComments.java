fdb23b8c0b1433ca64bb7c6178178bedfba4b9aa
Septian Yudha Nailendra <septianyudha21@if.unjani.ac.id>
2023-11-24 21:33:08 +0700

    Bad Comments Noise Comments

---
+++ Modul5/Bad Comments/NoiseComments.java
@@ -0,0 +1,11 @@
+*/
+protected AnnualDateRule() { }
+/** The day of the month. */ private int
+dayOfMonth;
+/**
+*Returns the day of themonth.
+*@return the day of themonth. 
+*/ 
+public int getDayOfMonth() { return
+dayOfMonth;
+}
