28a61a1f986b06a39944354dac0df290f5b6f9e2
Septian Yudha Nailendra <septianyudha21@if.unjani.ac.id>
2023-11-24 21:46:39 +0700

    Bad Formatting

---
+++ Modul5/Bad Formatting/FitnesseExpediter.java
@@ -0,0 +1,15 @@
+package Formatting;
+
+public class FitnesseExpediter implements ResponseSender {
+	private Socket socket;
+	private InputStream input;
+	private OutputStream output;
+	private Request request;
+	private Response response;
+	private FitNesseContext context;
+	protected long requestParsingTimeLimit;
+	private long requestProgress;
+	private long requestParsingDeadLine;
+	private boolean hasError;
+	...
+}
