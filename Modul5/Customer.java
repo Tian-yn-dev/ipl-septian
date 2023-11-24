19c440ebed0738ea84e41235af19afd5f52b0374
Septian Yudha Nailendra <septianyudha21@if.unjani.ac.id>
2023-11-24 20:53:38 +0700

    customer setelah di ubah

--- Modul5/Customer.java
+++ Modul5/Customer.java
@@ -2,9 +2,9 @@ package UsePronounceableNames;
 
 import java.util.Date;
 
-class DtaRcrd102 {
-	private Date genymdhms;
-	private Date modymdhms;
-	private final String pszqint = "102";
+public class Customer {
+	private Date generationTimestamp;
+	private Date modificationTimestamp;
+	private final String recordId = "102";
 	/* ... */
 }
